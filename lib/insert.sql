f11f50bd905997ab44471432a6b9657d7f4bfa8f
Michaela Brown <m_brownbrown@yahoo.com>
2023-02-06 17:55:11 -0800

    commit tree d52e309c8e20a765e46cb2aa075d478bb26856c7

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 13, 'M', 'Brown', 'Mean', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 3, 'M', 'White', 'Cute', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 7, 'F', 'Brown', 'Sleepy', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 35, 'M', 'Brown', 'Quiet', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 43, 'M', 'Black', 'Easy-going', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 5, 'M', 'Black', 'Mean', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 18, 'F', 'Brown', 'Mean', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 11, 'F', 'White', 'Unknown', 1);
